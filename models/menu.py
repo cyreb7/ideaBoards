72b93f2e377eed7600354ed9b5f4d37eedd97bcb
Bryce Newbury <bryce@hiddenworld.us>
2018-05-29 13:38:53 -0700

    Added Decks and Boards pages.

--- models/menu.py
+++ models/menu.py
@@ -29,10 +29,12 @@ response.google_analytics_id = None
 # ----------------------------------------------------------------------------------------------------------------------
 
 response.menu = [
-    (T('Home'), False, URL('default', 'index'), [])
+    (T('Home'), False, URL('default', 'index')),
+    (T('Decks'), False, URL('decks', 'index')),
+    (T('Boards'), False, URL('boards', 'index')),
 ]
 
-DEVELOPMENT_MENU = True
+DEVELOPMENT_MENU = False
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -49,12 +51,7 @@ def _():
     # useful links to internal and external resources
     # ------------------------------------------------------------------------------------------------------------------
     response.menu += [
-        (T('My Sites'), False, URL('admin', 'default', 'site')),
-        (T('Sample Menu 1'), False, '#', [
-            (T('Entry1'), False, URL('default', 'entry1')),
-            (T('Entry2'), False, URL('default', 'entry2')),
-        ]),
-        (T('Sample Menu 2'), False, URL('default', 'index'))
+        
     ]
 
 
