083d181fd3b9956713c93116964aae41efcc45b3
Bryce Newbury <bryce@hiddenworld.us>
2018-06-10 16:19:52 -0700

    load cards on the board page

--- static/js/boards_index.js
+++ static/js/boards_index.js
@@ -10,25 +10,105 @@ var app = function() {
             a.push(b[i]);
         }
     };
+    
+    // Enumerates an array.
+    var enumerate = function(v) { var k=0; return v.map(function(e) {e._idx = k++;});};
 
+/*
+------------------------------------------------------------------------------------
+ 
+------------------------------------------------------------------------------------
+*/
     self.calendar = function(){
         $( "#sampleCard" ).draggable();
     }
+    
+// Copped from decks_index.js
+// Really should be modularized
 
-    // Complete as needed.
+/*
+------------------------------------------------------------------------------------
+deck function
+------------------------------------------------------------------------------------
+*/
+
+    /*get all decks belonging to the signed in user*/
+    self.get_decks = function(){
+        $.get(get_decks_url,
+            function(decks){
+                //initialize all deck uploading status
+                for(var i=0; i<decks.length; i++){
+                    decks[i].is_uploading = false;
+                    decks[i].editing_deck = false;
+                }
+                enumerate(decks);
+                self.vue.curr_decks = decks;
+            }
+        )
+    }
+
+/*
+------------------------------------------------------------------------------------
+card functions
+------------------------------------------------------------------------------------
+*/
+
+    //show cards belonging to the given deck
+    self.get_cards = function(){
+        $.get(show_cards_url,
+            function (data) {
+                enumerate(data);
+                self.vue.curr_cards = data;
+                self.vue.delete_cart = [];
+                self.vue.caption_cart = [];
+            })
+    }
+
+
+/*
+------------------------------------------------------------------------------------
+Our Vue Object
+------------------------------------------------------------------------------------
+*/
     self.vue = new Vue({
         el: "#vue-div",
         delimiters: ['${', '}'],
         unsafeDelimiters: ['!{', '}'],
         data: {
+            curr_decks: [],         //storeall decks of signed in user
+            curr_cards: [],          //store all cards of signed in user
         },
         methods: {
-            calender: self.calendar
+            //deck functions
+            get_decks: self.get_decks,
+
+            //card functions
+            get_cards: self.get_cards,
         }
 
     });
 
-    self.calendar();
+/*
+------------------------------------------------------------------------------------
+initialize values like login status, decks, and cards when user
+first signs in
+------------------------------------------------------------------------------------
+*/
+    var initialize = function(){
+        $.get(login_status_url,
+            function (user) {
+                //get login status
+                self.vue.logged_in = (user != null);
+                //if user is logged in, fetch user decks and cards
+                if(self.vue.logged_in){
+                    self.get_decks();
+                    self.get_cards();
+                }
+            });
+    }();        
+
+    $("#vue-div").show();
+
     return self;
 };
 
